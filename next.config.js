f3c242cdc988a1375a9b67f9cb9d1bf20cb17a0e
theh0riz0n <77751911+theh0riz0n@users.noreply.github.com>
2025-09-17 23:31:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: ["app-build-manifest.json"],
 });
 
 const nextConfig = {
@@ -58,4 +59,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig); 
+module.exports = withPWA(nextConfig);
