667eba7178b6ad2119096485671bfd473495aca8
theh0riz0n <77751911+theh0riz0n@users.noreply.github.com>
2025-09-18 23:53:14 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ApiapProvider } from '@/context/ApiapContext';
+import { SnowflakesProvider } from '@/context/SnowflakesContext';
+import { LanguageProvider } from '@/context/LanguageContext';
+import { ThemeProvider } from '@/context/AccentColorContext';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { AnimatePresence } from 'framer-motion';
-import { useEffect } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ApiapProvider } from '@/context/ApiapContext';
-import { SnowflakesProvider } from '@/context/SnowflakesContext';
-import { ThemeProvider } from '@/context/AccentColorContext';
+import { useEffect } from 'react';
 import Snowflakes from '@/components/effects/Snowflakes';
 import ErrorBoundary from '@/components/ErrorBoundary';
 
@@ -48,7 +50,9 @@ if (typeof window !== 'undefined') {
   };
 }
 
-export default function App({ Component, pageProps, router }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
@@ -76,16 +80,21 @@ export default function App({ Component, pageProps, router }: AppProps) {
 
   return (
     <ErrorBoundary>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <QueryClientProvider client={queryClient}>
         <ApiapProvider>
           <ThemeProvider>
             <SnowflakesProvider>
-              <Snowflakes />
-              <AnimatePresence mode="wait" initial={true}>
-                <Component {...pageProps} key={router.route} />
-              </AnimatePresence>
-              <Analytics />
-              <SpeedInsights />
+              <LanguageProvider>
+                <Snowflakes />
+                <AnimatePresence mode="wait">
+                  <Component {...pageProps} key={router.route} />
+                </AnimatePresence>
+                <Analytics />
+                <SpeedInsights />
+              </LanguageProvider>
             </SnowflakesProvider>
           </ThemeProvider>
         </ApiapProvider>
@@ -93,3 +102,5 @@ export default function App({ Component, pageProps, router }: AppProps) {
     </ErrorBoundary>
   );
 }
+
+export default MyApp;
